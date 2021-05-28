305e8e04c51e4f75b71a674a1e54887ddd4c5004
artjitt9611 <jittaphon_jankham@cmu.ac.th>
2021-05-28 16:48:04 +0700

    05 homework 1 & 2

--- javascript.js
+++ javascript.js
@@ -1,5 +1,32 @@
-document.getElementById('ok').addEventListener('click',function(){
+
+document.getElementById('ok').addEventListener('click',function(e){
     
-    alert('ok is clicked')
+    alert('ok')
+  
 }
 )
+document.getElementById('cancel').addEventListener('click',function(e){
+    
+    alert('cancel')
+  
+}
+)
+
+document.getElementById('no').addEventListener('click',function(e){
+    
+    alert('no')
+  
+}
+)
+
+
+  
+ function showCoords(e){
+    console.log("event is fired on "+e.currentTarget.id+" X="+e.x+" Y="+e.y+
+    " offsetX="+e.offsetX+" offsetY="+e.offsetY+" clientX="+e.clientX+" clienty="+e.clientY+
+    " screenX="+e.screenX+" screenY="+e.screenY);
+ }
+
+ function clearCoor() {
+   console.log()
+  }
