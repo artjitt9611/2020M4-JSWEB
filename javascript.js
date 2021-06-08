51bc122a7495d6f80e0210ff03a2c0e8a0f019c2
artjitt9611 <jittaphon_jankham@cmu.ac.th>
2021-06-08 14:25:09 +0700

    06
    06

--- javascript.js
+++ javascript.js
@@ -1,32 +1,51 @@
 
-document.getElementById('ok').addEventListener('click',function(e){
-    
-    alert('ok')
-  
+document.getElementById('ok').addEventListener('click', function (e) {
+
+    let noElement = document.getElementById('no')
+    if (noElement.classList.contains('toggleOn')) {
+        noElement.classList.replace('toggleOn', 'toggleOff')
+    } else if (noElement.classList.contains('toggleOff')) {
+        noElement.classList.replace('toggleOff', 'toggleOn')
+    } else {
+        noElement.classList.add('toggleOn')
+    }
+    console.log(noElement)
+
 }
 )
-document.getElementById('cancel').addEventListener('click',function(e){
-    
-    alert('cancel')
-  
+document.getElementById('cancel').addEventListener('mousemove', function (e) {
+    if (e.shiftKey) {
+        console.log('x =' + e.clientX + 'y = ' + e.clientY)
+    }
+
 }
 )
 
-document.getElementById('no').addEventListener('click',function(e){
-    
+document.getElementById('no').addEventListener('click', function (e) {
+
     alert('no')
-  
+
 }
 )
 
+var borderTick = 100
+document.getElementById('cancel').style.border = `${borderTick}px soild  white`
 
-  
- function showCoords(e){
-    console.log("event is fired on "+e.currentTarget.id+" X="+e.x+" Y="+e.y+
-    " offsetX="+e.offsetX+" offsetY="+e.offsetY+" clientX="+e.clientX+" clienty="+e.clientY+
-    " screenX="+e.screenX+" screenY="+e.screenY);
- }
+var cc = document.getElementById('container')
 
- function clearCoor() {
-   console.log()
-  }
+var cancelElement = document.getElementById('cancel')
+cancelElement.addEventListener('mousemove', function (e) {
+    cancelElement.innerHTML = `<div class = "container-col" >
+    <div class ="flex-item">
+    no1
+    </div>
+    <div classs ="flex-item">
+    no2
+    </div>
+    </div>
+    `
+})
+
+cancelElement.addEventListener('mouseleave',function(){
+    cancelElement.innerHTML=`cancel`
+})
