8c0373b7aeecb30b2a5e2d7e313c95a0612cf367
artjitt9611 <jittaphon_jankham@cmu.ac.th>
2021-06-08 16:15:54 +0700

    07 test do it
    1 and 2

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 
-document.getElementById('ok').addEventListener('click', function (e) {
+/*document.getElementById('ok').addEventListener('click', function (e) {
 
     let noElement = document.getElementById('no')
     if (noElement.classList.contains('toggleOn')) {
@@ -13,6 +13,40 @@ document.getElementById('ok').addEventListener('click', function (e) {
 
 }
 )
+*/
+
+var margins = 20;
+var OkElement = document.getElementById('ok')
+OkElement.addEventListener('mousedown',function(){
+    margins = margins+25
+    document.getElementById('cancel').style.marginLeft = `${margins}px`
+    document.getElementById('cancel').style.marginRight = `${margins}px`
+    document.getElementById('no').style.marginLeft = `${margins}px`
+    document.getElementById('no').style.marginRight = `${margins}px`
+
+})
+
+
+var count = 0;
+document.getElementById('no').addEventListener('dblclick',function(e){ 
+     count++;
+     if(count == 1){
+      OkElement.innerHTML = `Ok`
+     }else if(count == 2){
+        OkElement.innerHTML= `วิชานี้`
+     }else if(count == 3){
+        OkElement.innerHTML = `ง่าย`
+     } else if(count == 4){
+       OkElement.innerHTML = `จริงๆนะ`    
+    
+     }else{
+         OkElement.innerHTML = `ok`
+         count = 0
+     }
+                        
+})
+
+
 document.getElementById('cancel').addEventListener('mousemove', function (e) {
     if (e.shiftKey) {
         console.log('x =' + e.clientX + 'y = ' + e.clientY)
@@ -21,19 +55,8 @@ document.getElementById('cancel').addEventListener('mousemove', function (e) {
 }
 )
 
-document.getElementById('no').addEventListener('click', function (e) {
 
-    alert('no')
-
-}
-)
-
-var borderTick = 100
-document.getElementById('cancel').style.border = `${borderTick}px soild  white`
-
-var cc = document.getElementById('container')
-
-var cancelElement = document.getElementById('cancel')
+/*var cancelElement = document.getElementById('cancel')
 cancelElement.addEventListener('mousemove', function (e) {
     cancelElement.innerHTML = `<div class = "container-col" >
     <div class ="flex-item">
@@ -49,3 +72,9 @@ cancelElement.addEventListener('mousemove', function (e) {
 cancelElement.addEventListener('mouseleave',function(){
     cancelElement.innerHTML=`cancel`
 })
+
+
+
+var borderTick = 100
+document.getElementById('cancel').style.border = `${borderTick}px soild  white`
+*/
